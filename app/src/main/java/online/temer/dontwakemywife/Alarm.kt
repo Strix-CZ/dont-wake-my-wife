784bdb13648d3f35a9a5e14adb67084b022f69c6
Jan Šimonek <jan.simonek@gmail.com>
2021-02-21 19:18:03 +0100

    Vibration

--- app/src/main/java/online/temer/dontwakemywife/Alarm.kt
+++ app/src/main/java/online/temer/dontwakemywife/Alarm.kt
@@ -1,13 +1,21 @@
 package online.temer.dontwakemywife
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.view.Window
 import android.view.WindowInsetsController
 import android.view.WindowManager
 import android.widget.Button
+import androidx.annotation.RequiresApi
 
 class Alarm : AppCompatActivity() {
+
+    var vibrator : Vibrator? = null
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -21,5 +29,29 @@ class Alarm : AppCompatActivity() {
         findViewById<Button>(R.id.buttonDismiss).setOnClickListener {
             finish()
         }
+
+        vibrate()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun vibrate() {
+        val amp = VibrationEffect.DEFAULT_AMPLITUDE
+        val timings    = longArrayOf(100, 200, 100, 200, 100, 5000)
+        val amplitudes =  intArrayOf(amp,   0, amp,   0, amp, 0)
+
+        val shortPulse = VibrationEffect.createWaveform(timings, amplitudes, 0)
+        vibrator = getSystemService<Vibrator>(Vibrator::class.java)
+        vibrator?.vibrate(shortPulse)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        vibrator?.cancel()
+        super.onSaveInstanceState(outState)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onDestroy() {
+        vibrator?.cancel()
+        super.onDestroy()
     }
 }
